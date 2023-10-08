c7df90cf091a2c1cba2b559f568e6c0c41efe32f
VIEIRA-Michel <michelvieira.pro@gmail.com>
2023-10-08 10:56:37 -0400

    implement dynamic search bar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import Header from "@/components/Header/Header";
+import Header from "@/components/header/Header";
 import "./globals.scss";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { roboto, montserrat } from "@/fonts";
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import MediaCard from "@/components/media-card/MediaCard";
 import styles from "./page.module.css";
+import Popular from "@/components/popular/Popular";
+import Genres from "@/components/genres/Genres";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      <MediaCard />
+      <Popular />
+      <Genres />
     </div>
   );
 }
